4c6ac6a2f8d596b4156964818da70163fe257542
NAKIGOE.ORG <godooou@gmail.com>
2022-09-01 15:46:43 +0500

    first commit with folder structures

---
+++ graph1_create/graph1.py
@@ -0,0 +1,18 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+#matplotlib.use('Agg')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sea = pd.read_csv('./input/raw_data.csv')
+
+sea.plot()
+
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
