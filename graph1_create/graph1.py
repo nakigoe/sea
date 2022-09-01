ebe0c8c8bb85c98f99180dd7ccab02cc5e08f75c
NAKIGOE.ORG <godooou@gmail.com>
2022-09-01 19:23:43 +0500

    Image saved

--- graph1_create/graph1.py
+++ graph1_create/graph1.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 sea = pd.read_csv('./input/raw_data.csv')
 
 sea.plot()
-
+plt.savefig("./output/test.svg", format="svg")
 plt.show()
 
 #Two  lines to make our compiler able to draw:
