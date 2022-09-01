085bb9693c792b43ad8e5b68a6f6008d3db1d3e6
NAKIGOE.ORG <godooou@gmail.com>
2022-09-01 20:08:05 +0500

    Graph file name with a time stamp ready

--- graph1_create/graph1.py
+++ graph1_create/graph1.py
@@ -1,6 +1,8 @@
 #Three lines to make our compiler able to draw:
 import sys
 import matplotlib
+import time
+from datetime import datetime, timedelta
 #matplotlib.use('Agg')
 
 import pandas as pd
@@ -9,7 +11,13 @@ import matplotlib.pyplot as plt
 sea = pd.read_csv('./input/raw_data.csv')
 
 sea.plot()
-plt.savefig("./output/test.svg", format="svg")
+
+#saving
+tr = datetime.utcnow() + timedelta(milliseconds=0.5) #correct time rounding trick
+timestr = tr.strftime("%Y%m%d%H%M%S%f")[:-3]
+plt.savefig("./output/graph1_" + timestr + ".svg", format="svg")
+
+#comment the following section out when moving the project to production:
 plt.show()
 
 #Two  lines to make our compiler able to draw:
