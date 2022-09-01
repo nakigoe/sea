c5d31dfe09b50b9f15fc6acba92b2a1d56370c2c
NAKIGOE.ORG <godooou@gmail.com>
2022-09-01 22:01:39 +0500

    cleaner fonts coding

--- graph1_create/graph1.py
+++ graph1_create/graph1.py
@@ -17,7 +17,6 @@ sea.plot()
 #plot formatting:
 fp = FontProperties(fname='./fonts/KaishoMCBK1Pro-DeBold.otf', size=20)
 fp_label = FontProperties(fname='./fonts/KaishoMCBK1Pro-DeBold.otf', size=14)
-rcParams['font.family'] = fp.get_name()
 
 plt.title("高知県の水産業の温度[℃]",fontproperties=fp)
 plt.ylabel('温度[℃]',fontproperties=fp_label)
