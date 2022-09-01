635b2e3d9e6109e5cc9ec5c4005dfd64e6c72d68
NAKIGOE.ORG <godooou@gmail.com>
2022-09-01 21:58:19 +0500

    awesome Japanese font

--- graph1_create/graph1.py
+++ graph1_create/graph1.py
@@ -3,17 +3,24 @@ import sys
 import matplotlib
 import time
 from datetime import datetime, timedelta
-#matplotlib.use('Agg')
+from matplotlib.font_manager import FontProperties
+from matplotlib import rcParams
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+#matplotlib.use('Agg')
 
 sea = pd.read_csv('./input/raw_data.csv')
 sea.plot()
 
 #plot formatting:
-plt.title("高知県の水産業の温度[℃]")
-plt.ylabel('温度[℃]')
+fp = FontProperties(fname='./fonts/KaishoMCBK1Pro-DeBold.otf', size=20)
+fp_label = FontProperties(fname='./fonts/KaishoMCBK1Pro-DeBold.otf', size=14)
+rcParams['font.family'] = fp.get_name()
+
+plt.title("高知県の水産業の温度[℃]",fontproperties=fp)
+plt.ylabel('温度[℃]',fontproperties=fp_label)
 
 #saving
 tr = datetime.utcnow() + timedelta(milliseconds=0.5) #correct time rounding trick
