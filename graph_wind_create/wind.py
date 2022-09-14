81601dea758eb772b0a17605871c9661c01626bc
NAKIGOE.ORG <godooou@gmail.com>
2022-09-14 23:42:52 +0500

    ready

--- graph_wind_create/wind.py
+++ graph_wind_create/wind.py
@@ -1,25 +1,27 @@
 #code by Maxim Angel, email: teachermaxim@gmail.com
-#You see the latest version at https://github.com/nakigoe/sea/blob/main/graph-wind-kaze/wind-kaze.py
+#You see the latest version at https://github.com/nakigoe/sea/blob/main/graph_wind_create/wind.py
 
 import pandas as pd
-import numpy as np
 from svgmanip import Element
 
-sea = pd.read_csv('input/raw_data.csv')
-marker_x=sea['x']
-marker_y=sea['y']
-direction=-sea['deg'] #the app rotates clockwise, but we want counterclockwise rotation counting from the South (if the South in the coming data = 0)
+def make_graph_wind():
+    sea = pd.read_csv('input/raw_data.csv')
+    marker_x=sea['x']
+    marker_y=sea['y']
+    direction=-sea['deg'] #the app rotates clockwise, but we want counterclockwise rotation counting from the South (if the South in the coming data = 0)
 
-#output file and graphics' placement
-output = Element(960, 700)  # size of the output file.
+    #output file and graphics' placement
+    output = Element(960, 700)  # size of the output file.
 
-base = Element('img/map.svg') #increase the map size directly inside the SVG file to Your liking!
-output.placeat(base, 0, 0) #place the map at the top left corner of the output SVG composed image
+    base = Element('img/map.svg') #increase the map size directly inside the SVG file to Your liking!
+    output.placeat(base, 0, 0) #place the map at the top left corner of the output SVG composed image
 
-wind_array=[]
-for i in range(len(sea.index)):
-    wind = Element('img/wind.svg').scale(0.25).rotate(direction[i], 48, 20) #these numbers are marker's rotation points relative to the marker figure!!!
-    wind_array.append(wind)
-    output.placeat(wind_array[i], int(marker_x[i]), int(marker_y[i]))
+    wind_array=[]
+    for i in range(len(sea.index)):
+        wind = Element('img/wind.svg').scale(0.25).rotate(direction[i], 48, 20) #these numbers are marker's rotation points relative to the marker figure!!!
+        wind_array.append(wind)
+        output.placeat(wind_array[i], int(marker_x[i]), int(marker_y[i]))
 
-output.dump('output/output.svg')
+    output.dump('output/output.svg')
+
+make_graph_wind()
