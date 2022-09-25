5640b13bed562a6c9d739ecef68aae99de583614
Nakigoe <nakigoe@pm.me>
2022-09-25 18:37:23 +0500

    save path

--- upload_form/myapp/models.py
+++ upload_form/myapp/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Document(models.Model):
-    docfile = models.FileField(upload_to='/home/ec2-user/test/pdf') #/documents/%Y/%m/%d
+    docfile = models.FileField(upload_to='home/ec2-user/test/pdf') #/documents/%Y/%m/%d
