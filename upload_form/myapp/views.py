5d96d65685424c5f09c2654c39f6367c8d69f121
Nakigoe <nakigoe@pm.me>
2022-09-25 18:24:34 +0500

    Japanese text

--- upload_form/myapp/views.py
+++ upload_form/myapp/views.py
@@ -4,7 +4,7 @@ from .forms import DocumentForm
 
 
 def my_view(request):
-    print(f"Great! You're using Python 3.6+. If you fail here, use the right version.")
+    print(f"貴方は Python 3.6+ を使用しています。ここで失敗した場合は、正しいバージョンを使用してください。")
     message = 'どんな数でのファイルをアップロードしてください！'
     # Handle file upload
     if request.method == 'POST':
@@ -16,7 +16,7 @@ def my_view(request):
             # Redirect to the document list after POST
             return redirect('my-view')
         else:
-            message = 'The form is not valid. Fix the following error:'
+            message = 'フォームが無効でございます。このエラーを修正してください：'
     else:
         form = DocumentForm()  # An empty, unbound form
 
