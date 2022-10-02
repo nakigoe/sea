1e2e347ab9e17f7dfebbaafa03de858e3ca72d52
Nakigoe <nakigoe@pm.me>
2022-10-02 15:12:39 +0500

    testing file preview before upload

--- upload_form/myapp/views.py
+++ upload_form/myapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import redirect, render
+from django.views.decorators.clickjacking import xframe_options_exempt
 from .models import Document
 from .forms import DocumentForm
 
-from django.views.decorators.clickjacking import xframe_options_exempt
 
 @xframe_options_exempt
 def my_view(request):
-    print(f"貴方は Python 3.6+ を使用しています。ここで失敗した場合は、正しいバージョンを使用してください。")
+    print("貴方は Python 3.6+ を使用しています。ここで失敗した場合は、正しいバージョンを使用してください。")
     message = 'どんな数でのファイルをアップロードして下さい！'
     # Handle file upload
     if request.method == 'POST':
@@ -30,3 +30,10 @@ def my_view(request):
     test = render(request, 'list.html', context)
     test ['Content-Security-Policy'] = "frame-ancestors 'self' http://127.0.0.1:8000/"
     return test
+
+from django import template
+register = template.Library()
+
+@register.simple_tag
+def go_to_url(url):
+    return "window.location='" + url +"'; return false;"
