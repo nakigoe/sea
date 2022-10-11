099d17bfcf158da58174105509c790f2d83841ea
Nakigoe <nakigoe@pm.me>
2022-10-11 22:32:13 +0500

    names

--- upload_form/myapp/services.py
+++ upload_form/myapp/services.py
@@ -3,5 +3,5 @@ import subprocess
 
 def send_to_server(local_path):
     database_items_path="D:/Docs/Website/sea/upload_form"
-    complete_item_path= database_items_path + local_path
-    subprocess.run(["scp", "-i", "C:/Users/HP/.ssh/mykey.pem", complete_item_path, "ec2-user@3.115.9.253:/home/ec2-user/test/pdf/"])
+    absolute_path= database_items_path + local_path
+    subprocess.run(["scp", "-i", "C:/Users/HP/.ssh/mykey.pem", absolute_path, "ec2-user@3.115.9.253:/home/ec2-user/test/pdf/"])
