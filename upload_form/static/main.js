967577c1795fead2501d34c146ef51b12e886a06
Nakigoe <nakigoe@pm.me>
2022-10-16 19:35:59 +0500

    buttons unified

--- upload_form/static/main.js
+++ upload_form/static/main.js
@@ -1,7 +1,25 @@
 const fileSelect = document.getElementById("fileSelect"),
     fileElem = document.getElementById("fileElem"),
     fileList = document.getElementById("fileList"),
-    fileDrop = document.getElementById("dropZone");
+    dropbox = document.getElementById("dropZone");
+
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+// dropbox.addEventListener("drop", drop, false);
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+dropbox.addEventListener("drop", (e) => {
+  if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault(); // prevent navigation to "#"
+}, false);
 
 fileSelect.addEventListener("click", (e) => {
   if (fileElem) {
@@ -10,30 +28,14 @@ fileSelect.addEventListener("click", (e) => {
   e.preventDefault(); // prevent navigation to "#"
 }, false);
 
-fileDrop.addEventListener("drop", (e) => {
-  if (fileElem) {
-    fileElem.click();
-  }
-  e.preventDefault(); // prevent navigation to "#"
-}, false);
-fileDrop.addEventListener("dragenter", (e) => {
-  e.preventDefault(); // prevent navigation to "#"
-}, false);
-fileDrop.addEventListener("dragover", (e) => {
-  e.preventDefault(); // prevent navigation to "#"
-}, false);
-
 fileElem.addEventListener("change", handleFiles, false);
 
 function formatBytes(bytes, decimals = 2) {
   if (!+bytes) return '0 Bytes'
-
   const k = 1024
   const dm = decimals < 0 ? 0 : decimals
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-
   const i = Math.floor(Math.log(bytes) / Math.log(k))
-
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
 
