b29cadb91f74e15d7b36afa502a37f92689114c9
NAKIGOE.ORG <godooou@gmail.com>
2022-10-23 14:15:08 +0500

    new instructions

--- graph_wind_create/wind.py
+++ graph_wind_create/wind.py
@@ -1,8 +1,10 @@
-#code by Maxim Angel, email: teachermaxim@gmail.com
-#You can see the latest version at https://github.com/nakigoe/sea/blob/main/graph_wind_create/wind.py
-
-#------ basic usage ------ Python Library from https://github.com/CrazyPython/svgmanip
 '''
+code by Maxim Angel, email: teachermaxim@gmail.com
+You can see the latest version at https://github.com/nakigoe/sea/blob/main/graph_wind_create/wind.py
+
+------------------------------------ basic usage ---------------------------------- 
+Python Library from https://github.com/CrazyPython/svgmanip
+
 output = Element(384, 356)  # size of the output file, look up the SVG code to get correct proportions!
 
 map = Element('assets/map.svg')
@@ -15,11 +17,37 @@ output.placeat(marker2, 100.9, 50)
 
 output.dump('output.svg')
 output.save_as_png('output.png', 1024) #does not work under Windows, utilize PYVIPS library and system PATH for PYVIPS DLLs
-'''
 
-# --------------------------- convert SVG to PNG under Windows: --------------------
-# https://github.com/libvips/build-win64-mxe/releases
-# unzip and add the BIN folder with DLL's to the system PATH
+************************************** convert SVG to PNG *********************************************************
+
+=============================== Install Libvips under Windows: =====================================
+https://github.com/libvips/build-win64-mxe/releases
+unzip and add the BIN folder with DLL's to the system PATH
+
+=============================== Install Libvips in Red Hat Enterprise Linux 9 ======================
+-------------------------------------- Setup Python ----------------------------------------
+$ su root
+# dnf install python-requests
+# dnf install python-pip
+# dnf install python-wheel
+
+-------------------------------------- Install Libvips ----------------------------------------
+# subscription-manager repos --enable codeready-builder-for-rhel-9-x86_64-rpms
+# subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms
+# dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+# dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
+# dnf config-manager --set-enabled remi
+# dnf install vips vips-devel vips-tools
+# dnf upgrade
+
+--------------------------------- Install the app dependencies -------------------------------
+$ pip install pyvips
+$ pip install pandas
+$ pip install svgutils
+
+-------------------------------------- Run the wind.py app -------------------------------------
+$ python wind.py
+'''
 import pyvips 
 
 import pandas as pd
@@ -30,7 +58,7 @@ def make_graph_wind():
     original_map_width = 535 #532.1  #SVG hardcoded numbers, open the SVG file!
     original_map_height = 431 #417.6 
     map_scale_up = 2 #increase the output dimensions to your liking by this parameter!
-    map_width = original_map_width * map_scale_up
+    map_width = original_map_width * map_scale_up #PNG 1070px
     map_height = original_map_height * map_scale_up
     
     sea = pd.read_csv('input/raw_data.csv')
@@ -39,7 +67,7 @@ def make_graph_wind():
     direction = -sea['deg'] #the app rotates clockwise, but we want counterclockwise rotation counting from the South (if the South in the coming data = 0)
     
     #output file and graphics' placement
-    output = Element(map_width, map_height)  # size of the output file.
+    output = Element(map_width, map_height)  # size of the output file. 
 
     base = Element('img/map.svg').scale(map_scale_up)
     output.placeat(base, 0, 0) #place the map at the top left corner of the output SVG composed image
